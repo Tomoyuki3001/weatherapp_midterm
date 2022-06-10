6ced8c0bd78dec580e47379919babb0df5cfeea3
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-06-10 14:43:42 -0700

    made two parts

--- Js/daysHoursWheather.js
+++ Js/daysHoursWheather.js
@@ -22,71 +22,56 @@ function showWheatherData(data) {
     }
   });
 
-  console.log(dateObject);
-
-  console.log(daysNewWheather[0].main.temp);
-
-  for (let i = 0; i < 6; i++) {
+  let daysNewArray = Object.keys(dateObject);
+  daysNewArray.forEach((day) => {
+    const dayData = dateObject[day][0];
     const daysCard = document.createElement("div");
     daysCard.classList.add("days_card");
-
-    let daysNewArray = Object.keys(dateObject);
-    let daysNewWheather = dateObject[daysNewArray[i]];
-
-    let temp = daysNewWheather.main.temp;
-    console.log(temp);
+    let temp = dayData.main.temp;
     let newTemp = Math.trunc(temp);
-    let tempFeels = newDaysArray[i].main.feels_like;
+    let tempFeels = dayData.main.feels_like;
     let newTempFeels = Math.trunc(tempFeels);
-    let pop = Math.floor(newDaysArray[i].pop * 100);
-
+    let pop = Math.floor(dayData.pop * 100);
     daysCard.innerHTML = `
-    <div class="days_day">${moment(newDaysArray[i].dt * 1000).format(
-      "ddd"
-    )}</div>
-      <img
-        src="http://openweathermap.org/img/wn/${
-          newDaysArray[i].weather[0].icon
-        }@2x.png"
-        alt="weather icon"
-        class="days_weather-icon"
-      />
-      <div class="days_temp">${newTemp}&#176</div>
-      <div class="days_feels">Feels like</div>
-      <div class="days_temp_feels">${newTempFeels}&#176</div>
-      
-      <div class="days_pop"><span class="iconify days_pop_icon" data-icon="bi:cloud-rain-heavy" style="color: white; font-size: 17px;"></span> ${pop}%</div>
-
-    `;
+    <div class="days_day">${moment(dayData.dt * 1000).format("ddd")}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            dayData.weather[0].icon
+          }@2x.png"
+          alt="weather icon"
+          class="days_weather-icon"
+        />
+        <div class="days_temp">${newTemp}&#176</div>
+        <div class="days_feels">Feels like</div>
+        <div class="days_temp_feels">${newTempFeels}&#176</div>
+  
+        <div class="days_pop"><span class="iconify days_pop_icon" data-icon="bi:cloud-rain-heavy" style="color: white; font-size: 17px;"></span> ${pop}%</div>
+  
+      `;
     weatherForecastE1.appendChild(daysCard);
-  }
-  const daysCardArray = document.querySelectorAll(".days_card");
-  console.log(daysCardArray);
+  });
 
-  const hoursData = data.list;
-  for (let i = 0; i < 8; i++) {
+  let hoursNewData = dateObject[daysNewArray[0]];
+  console.log("hoursDAta", hoursNewData);
+
+  hoursNewData.forEach((hour) => {
     const hoursCard = document.createElement("div");
     hoursCard.classList.add("hours_box");
 
-    let tempHours = hoursData[i].main.temp;
+    let tempHours = hour.main.temp;
     let newTempHours = Math.trunc(tempHours);
-    let tempFeelsHours = hoursData[i].main.feels_like;
+    let tempFeelsHours = hour.main.feels_like;
     let newTempFeelsHours = Math.trunc(tempFeelsHours);
-    let popHours = Math.floor(hoursData[i].pop * 100);
-    let date = hoursData[i].dt_txt;
-    let newDate = date.slice("5");
-    let newDateEnd = newDate.slice("0", "-3");
-    let changeDate = newDateEnd.replace("-", "/");
-    let dateFinal = changeDate.slice("0", "-5");
-    let timeFinal = changeDate.slice("5");
-
+    let popHours = Math.floor(hour.pop * 100);
+    let date = hour.dt_date;
+    let time = hour.dt_time;
     hoursCard.innerHTML = `
     <div class="hours_date_box">
-    <div class="hours_date">${dateFinal}</div>
-    <div class="hours_time">${timeFinal}</div>
+    <div class="hours_date">${date}</div>
+    <div class="hours_time">${time}</div>
     </div>
     <div><img
-    src="http://openweathermap.org/img/wn/${hoursData[i].weather[0].icon}@2x.png"
+    src="http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png"
     alt="weather icon"
     class="hours_weather-icon"
   /></div>
@@ -98,7 +83,9 @@ function showWheatherData(data) {
    <div class="hours_pop"><span class="iconify days_pop_icon" data-icon="bi:cloud-rain-heavy" style="color: white; font-size: 17px;"></span>${popHours}%</div>
     `;
     weatherForecastE2.appendChild(hoursCard);
-  }
+  });
+
+  // }
 }
 
 export default showWheatherData;
